68742fdaf2030c3e3065d1183c1dac420d010065
nuri park <nuripark@MacBookPro.local>
2023-02-27 23:38:49 +0900

    코드 스플리팅과 이모션

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import loadable from '@loadable/component';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import LogIn from '@pages/LogIn';
-import SignUp from '@pages/SignUp';
+const LogIn = loadable(() => import('@pages/LogIn'));
+//import LogIn from '@pages/LogIn';
+const SignUp = loadable(() => import('@pages/SignUp'));
+//import SignUp from '@pages/SignUp';
 
 const App = () => {
   return (
--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Form, Label, Input, LinkContainer, Button, Header } from './styles';
 
 const SignUp = () => {
-  return <div>회원가입</div>;
+  return (
+    <div>
+      <Header>Sleact</Header>
+    </div>
+  );
 };
 
 export default SignUp;
